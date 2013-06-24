e89c3fc7af40669609f326695156bab27690d4b5
Per Arnborg <per_arnborg@hotmail.com>
2013-06-24 11:27:32 +0200

    Add ignore file

--- config/telldus.php
+++ config/telldus.php
@@ -1,9 +1,9 @@
 <?php
 class telldus {
-  const publicKey = '';
-  const privateKey = '';
-  const token = '';
-  const tokenSecret = '';
+  const publicKey = 'FEHUVEW84RAFR5SP22RABURUPHAFRUNU';
+  const privateKey = 'ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF';
+  const token = 'f0a9e28bd7c5f4c0501e7a127ecaa239051c80273';
+  const tokenSecret = '0b06271848dedbb802505525b3545105';
   const testAccountEmail = 'stefan@nada.kth.se';
 }
 ?>
